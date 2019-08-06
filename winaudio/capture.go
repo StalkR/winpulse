e058f95a4023a1dfb4e99aced5d182b093e6b2fe
StalkR <stalkr@stalkr.net>
2019-08-06 15:05:11 +0200

    winaudio: annotate errors

--- winaudio/capture.go
+++ winaudio/capture.go
@@ -19,76 +19,76 @@ import (
 // Format is float32 little-endian, stereo 16-bit 44100 Hz (CD Quality).
 func Capture(ctx context.Context, sink io.Writer) error {
   if err := ole.CoInitializeEx(0, ole.COINIT_MULTITHREADED); err != nil {
-    return err
+    return fmt.Errorf("winaudio: CoInitializeEx: %v", err)
   }
   defer ole.CoUninitialize()
 
   var de *wca.IMMDeviceEnumerator
   if err := wca.CoCreateInstance(wca.CLSID_MMDeviceEnumerator, 0, wca.CLSCTX_ALL, wca.IID_IMMDeviceEnumerator, &de); err != nil {
-    return err
+    return fmt.Errorf("winaudio: CoCreateInstance: %v", err)
   }
   defer de.Release()
 
   var mmd *wca.IMMDevice
   if err := de.GetDefaultAudioEndpoint(wca.ERender, wca.EConsole, &mmd); err != nil {
-    return err
+    return fmt.Errorf("winaudio: GetDefaultAudioEndpoint: %v", err)
   }
   defer mmd.Release()
 
   var ac *wca.IAudioClient
   if err := mmd.Activate(wca.IID_IAudioClient, wca.CLSCTX_ALL, nil, &ac); err != nil {
-    return err
+    return fmt.Errorf("winaudio: Activate: %v", err)
   }
   defer ac.Release()
 
   var wfx *wca.WAVEFORMATEX
   if err := ac.GetMixFormat(&wfx); err != nil {
-    return err
+    return fmt.Errorf("winaudio: GetMixFormat: %v", err)
   }
   defer ole.CoTaskMemFree(uintptr(unsafe.Pointer(wfx)))
 
   const WAVE_FORMAT_EXTENSIBLE = 0xfffe // float32 little-endian
   if wfx.WFormatTag != WAVE_FORMAT_EXTENSIBLE {
-    return fmt.Errorf("format: expected WAVE_FORMAT_EXTENSIBLE but got %v", wfx.WFormatTag)
+    return fmt.Errorf("winaudio: expected WAVE_FORMAT_EXTENSIBLE but got %v", wfx.WFormatTag)
   }
   if channels := uint16(2); wfx.NChannels != channels {
-    return fmt.Errorf("format: expected %v channels but got %v", channels, wfx.NChannels)
+    return fmt.Errorf("winaudio: expected %v channels but got %v", channels, wfx.NChannels)
   }
   if bits := uint16(16); wfx.WBitsPerSample/wfx.NChannels != bits {
-    return fmt.Errorf("format: expected %v-bit but got %v-bit", bits, wfx.WBitsPerSample)
+    return fmt.Errorf("winaudio: expected %v-bit but got %v-bit", bits, wfx.WBitsPerSample)
   }
   if freq := uint32(44100); wfx.NSamplesPerSec != freq {
-    return fmt.Errorf("format: expected %v Hz but got %v Hz", freq, wfx.NSamplesPerSec)
+    return fmt.Errorf("winaudio: expected %v Hz but got %v Hz", freq, wfx.NSamplesPerSec)
   }
 
   var defaultPeriod wca.REFERENCE_TIME
   var minimumPeriod wca.REFERENCE_TIME
   if err := ac.GetDevicePeriod(&defaultPeriod, &minimumPeriod); err != nil {
-    return err
+    return fmt.Errorf("winaudio: GetDevicePeriod: %v", err)
   }
   latency := time.Duration(int(defaultPeriod) * 100)
 
   const hnsBufferDuration = 0
   const hnsPeriodicity = 0
   if err := ac.Initialize(wca.AUDCLNT_SHAREMODE_SHARED, wca.AUDCLNT_STREAMFLAGS_LOOPBACK, hnsBufferDuration, hnsPeriodicity, wfx, nil); err != nil {
-    return err
+    return fmt.Errorf("winaudio: Initialize: %v", err)
   }
 
   var acc *wca.IAudioCaptureClient
   if err := ac.GetService(wca.IID_IAudioCaptureClient, &acc); err != nil {
-    return err
+    return fmt.Errorf("winaudio: GetService: %v", err)
   }
   defer acc.Release()
 
   // register with MMCSS
   revertMm, err := setMmThreadCharacteristics("Audio")
   if err != nil {
-    return err
+    return fmt.Errorf("winaudio: setMmThreadCharacteristics: %v", err)
   }
   defer revertMm()
 
   if err := ac.Start(); err != nil {
-    return err
+    return fmt.Errorf("winaudio: Start: %v", err)
   }
   defer ac.Stop()
 
@@ -101,7 +101,7 @@ func Capture(ctx context.Context, sink io.Writer) error {
     for {
       var packetLength uint32
       if err := acc.GetNextPacketSize(&packetLength); err != nil {
-        return err
+        return fmt.Errorf("winaudio: GetNextPacketSize: %v", err)
       }
       if packetLength == 0 {
         break
@@ -111,7 +111,7 @@ func Capture(ctx context.Context, sink io.Writer) error {
       var numFramesAvailable uint32
       var flags uint32
       if err := acc.GetBuffer(&data, &numFramesAvailable, &flags, nil, nil); err != nil {
-        return err
+        return fmt.Errorf("winaudio: GetBuffer: %v", err)
       }
       // now careful not to return/break early and skipping ReleaseBuffer
 
@@ -134,11 +134,11 @@ func Capture(ctx context.Context, sink io.Writer) error {
       _, writeErr := sink.Write(b)
 
       if err := acc.ReleaseBuffer(numFramesAvailable); err != nil {
-        return err
+        return fmt.Errorf("winaudio: ReleaseBuffer: %v", err)
       }
 
       if writeErr != nil {
-        return writeErr
+        return fmt.Errorf("winaudio: write: %v", err)
       }
     }
     time.Sleep(latency / 2)
@@ -160,12 +160,12 @@ func setMmThreadCharacteristics(name string) (func() error, error) {
   taskIndex := 0
   avHandle, _, _ := procAvSetMmThreadCharacteristicsA.Call(uintptr(unsafe.Pointer(taskName)), uintptr(unsafe.Pointer(&taskIndex)))
   if avHandle == 0 {
-    return nil, fmt.Errorf("AvSetMmThreadCharacteristicsA returned %v", avHandle)
+    return nil, fmt.Errorf("winaudio: AvSetMmThreadCharacteristicsA: %v", avHandle)
   }
   return func() error {
     r, _, _ := procAvRevertMmThreadCharacteristics.Call(avHandle)
     if r == 0 {
-      return fmt.Errorf("AvRevertMmThreadCharacteristics returned %v", r)
+      return fmt.Errorf("winaudio: AvRevertMmThreadCharacteristics: %v", r)
     }
     return nil
   }, nil
